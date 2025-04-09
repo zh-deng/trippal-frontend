142e288dfc728da036db3431600d340dec3fa25f
zh-deng <heromemo@web.de>
2025-04-09 15:00:14 +0200

    restructure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import { Route, Routes } from "react-router"
-import Navbar from "./components/Navbar/Navbar"
-import Home from "./pages/Home/Home"
-import Registration from "./pages/Registration/Registration"
-import NotFound from "./pages/NotFound/NotFound"
-import Demo from "./pages/Demo/Demo"
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Registration from "./pages/Registration/Registration";
+import NotFound from "./pages/NotFound/NotFound";
+import Demo from "./pages/Demo/Demo";
 
 function App() {
-
-  return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="demo" element={<Demo/>}/>
-        <Route path="registration" element={<Registration/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </div>
-  )
+	return (
+		<div className="app">
+			<Navbar />
+			<div className="app__pages">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="demo" element={<Demo />} />
+					<Route path="registration" element={<Registration />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
