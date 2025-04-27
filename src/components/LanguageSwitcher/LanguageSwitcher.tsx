f692b865a6142add8fdf5a3a60fb94061962da6d
zh-deng <heromemo@web.de>
2025-04-27 19:00:16 +0200

    update language switcher

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,17 +1,20 @@
+import { useState } from "react";
 import "./LanguageSwitcher.scss"
 import { useTranslation } from 'react-i18next';
 
 function LanguageSwitcher() {
   const { i18n } = useTranslation();
+  const [activeLanguage, setActiveLanguage] = useState<string>("de")
 
   const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
+    setActiveLanguage(lng);
   };
 
   return (
     <div className="language-switcher">
-      <button onClick={() => changeLanguage('en')}>EN</button>
-      <button onClick={() => changeLanguage('de')}>DE</button>
+      <button className={activeLanguage === "en" ? "language-active" : ""} onClick={() => changeLanguage('en')}>EN</button>
+      <button className={activeLanguage === "de" ? "language-active" : ""} onClick={() => changeLanguage('de')}>DE</button>
     </div>
   );
 }
