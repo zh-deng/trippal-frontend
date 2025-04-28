8e6c5e235ce228aa1dc1b751ef963182170198c2
zh-deng <heromemo@web.de>
2025-04-28 22:45:43 +0200

    add dashboard tabmenu component

---
+++ src/components/DashboardTabMenu/DashboardTabMenu.tsx
@@ -0,0 +1,23 @@
+import "./DashboardTabMenu.scss";
+import { BsPlusLg } from "react-icons/bs";
+import { SlOptionsVertical } from "react-icons/sl";
+
+export const DashboardTabMenu = () => {
+	const items: string[] = ["TRIP 1", "TRIP 2", "TRIP 3", "TRIP 4"];
+
+	return (
+		<div className="dashboard-tabmenu">
+			{items.map((item) => (
+				<div className="tabmenu-item">
+					<div className="tabmenu-item-name">{item}</div>
+					<div className="tabmenu-item-options">
+						<SlOptionsVertical size={16} />
+					</div>
+				</div>
+			))}
+			<div className="tabmenu-add">
+				<BsPlusLg size={24} />
+			</div>
+		</div>
+	);
+};
