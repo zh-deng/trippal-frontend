8b02f1c1929d4a9b010823adc62bbfbe3fd05f27
zh-deng <heromemo@web.de>
2025-04-29 22:05:41 +0200

    fix some bugs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router";
 import "./Navbar.scss";
-import Text from "../Text/Text";
+import {Text} from "../Text/Text";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 import logo from "../../assets/logo.png";
 
