072cc8ed8fe463a365d6f418ccdc2a310741de6c
zh-deng <heromemo@web.de>
2025-04-30 20:26:01 +0200

    add redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/main.scss";
 import { BrowserRouter } from "react-router";
-import './i18n';
+import "./i18n";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./state/store.ts";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</StrictMode>
 );
