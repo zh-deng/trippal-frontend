2aa99c2b79bb694f6fe8351fd9b5218f3f666d6f
zh-deng <heromemo@web.de>
2025-05-05 17:53:26 +0200

    modify dashboard input component

--- src/components/DashboardInput/DashboardInput.tsx
+++ src/components/DashboardInput/DashboardInput.tsx
@@ -2,8 +2,10 @@ import { ChangeEvent, useRef, useState } from "react";
 import { Text } from "../Text/Text";
 import "./DashboardInput.scss";
 import { RxCross2 } from "react-icons/rx";
+import { useTranslation } from "react-i18next";
 
 export const DashboardInput = () => {
+	const { t } = useTranslation();
 	const [text, setText] = useState<string>("");
 	const [uploadedFiles, setUploadedFiles] = useState<string[]>([]);
 	const fileInputRef = useRef<HTMLInputElement>(null);
@@ -29,41 +31,44 @@ export const DashboardInput = () => {
 
 	return (
 		<form className="dashboard-input">
-			<div className="input-date">
-				<label>
-					<Text content={"Datum"} />
-				</label>
-				<input type="date" name="date" />
+			<div className="input-container-left">
+				<div className="input-date">
+					<label>
+						<Text content={"Datum"} />
+					</label>
+					<input type="date" name="date" />
+				</div>
+				<div className="input-notes">
+					<label htmlFor="textarea">
+						<Text content={"dashboard.center.inputs.notes"} />
+					</label>
+					<textarea
+						id="textarea"
+						value={text}
+						onChange={handleTextareaChange}
+						rows={9}
+						style={{ resize: "none" }}
+						placeholder={t("dashboard.center.inputs.notesPlaceholder")}
+					/>
+				</div>
 			</div>
-			<div className="input-notes">
-				<label htmlFor="textarea">
-					<Text content={"Notes"} />
-				</label>
-				<textarea
-					id="textarea"
-					value={text}
-					onChange={handleTextareaChange}
-					rows={5}
-					cols={40}
-					style={{ resize: "none" }}
-					placeholder="Notes"
-				/>
-			</div>
-			<div className="input-uploads">
-				<label>
-					<Text content={"dashboard.center.inputs.uploads"} />
-				</label>
-				<input
-					type="file"
-					accept=".pdf,.jpg,.jpeg,.png,.txt"
-					onChange={handleFileChange}
-					ref={fileInputRef}
-				/>
-
-				<div>
-					<ul>
+			<div className="input-container-right">
+				<div className="input-uploads">
+					<div className="input-uploads-info">
+						<Text content={"Füge Buchungen, Belege und Tickets hinzu."} />
+						<input
+							type="file"
+							accept=".pdf,.jpg,.jpeg,.png,.txt"
+							onChange={handleFileChange}
+							ref={fileInputRef}
+						/>
+						<label>
+							<Text content={"dashboard.center.inputs.uploads"} />
+						</label>
+					</div>
+					<ul className="upload-container">
 						{uploadedFiles.map((name, index) => (
-							<li className="upload-item" key={index}>
+							<li className={`upload-item ${index%2 === 0 ? "odd-color": "even-color"}`} key={index}>
 								<button onClick={() => handleRemoveUpload(index)}>
 									<RxCross2 />
 								</button>
@@ -71,6 +76,11 @@ export const DashboardInput = () => {
 							</li>
 						))}
 					</ul>
+				</div>
+				<div className="input-save-button">
+					<button>
+						<Text content={"dashboard.center.inputs.submit"} />
+					</button>
 				</div>
 			</div>
 		</form>
