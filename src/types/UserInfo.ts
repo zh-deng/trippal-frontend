709fbd127930c0ae63132c8e45c89014c3057fa4
zh-deng <heromemo@web.de>
2025-05-10 17:14:39 +0200

    add userinfo entity

---
+++ src/types/UserInfo.ts
@@ -0,0 +1,13 @@
+export type RegisterUserInput = {
+  name: string;
+  email: string;
+  password: string;
+  roles: string;
+};
+
+export type UserInfo = {
+  id: number;
+  name: string;
+  email: string;
+  roles: string;
+}
