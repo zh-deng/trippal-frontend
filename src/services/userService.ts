531140e68fd4bc8f9dde2e533f31653cfb8d7dfb
zh-deng <heromemo@web.de>
2025-05-11 17:22:30 +0200

    add new requests for login and logout to service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+import { LoginData } from "../dtos/loginData.dto";
 import { RegisterUserInput, UserInfo } from "../types/UserInfo";
 
 
@@ -7,7 +8,6 @@ export async function registerUser(user: RegisterUserInput): Promise<UserInfo> {
     headers: {
       "Content-Type": "application/json",
     },
-    mode: 'cors',
     body: JSON.stringify(user),
   });
 
@@ -18,4 +18,44 @@ export async function registerUser(user: RegisterUserInput): Promise<UserInfo> {
 
   const savedUser: UserInfo = await response.json();
   return savedUser;
+}
+
+export async function loginUser(loginData: LoginData): Promise<UserInfo> {
+  const response = await fetch(`${import.meta.env.VITE_BACKEND_BASEURL}/auth/login`, {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: 'cors',
+    body: JSON.stringify(loginData),
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`Login failed: ${errorText}`);
+  }
+
+  const savedUser: UserInfo = await response.json();
+
+  return savedUser
+}
+
+export async function logoutUser() {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_BASEURL}/auth/logout`, {
+            method: 'POST',
+            credentials: 'include',
+        });
+
+        if (response.ok) {
+            console.log("Logged out successfully!");
+            return Promise.resolve("Logout successful");
+        } else {
+            return Promise.reject("Failed to log out");
+        }
+    } catch (error) {
+        console.error("Error during logout:", error);
+        return Promise.reject("Logout error");
+    }
 }
