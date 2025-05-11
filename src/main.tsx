7e621cc0a2656bbd5ed22cac4adcfc35ced86026
zh-deng <heromemo@web.de>
2025-05-11 18:42:30 +0200

    fix minor bugs

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { BrowserRouter } from "react-router";
 import "./i18n";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./state/store.ts";
+import { persistor, store } from "./state/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Provider store={store}>
-				<App />
+				<PersistGate loading={null} persistor={persistor}>
+					<App />
+				</PersistGate>
 			</Provider>
 		</BrowserRouter>
 	</StrictMode>
