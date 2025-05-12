89adbfcd4d33ebd8080e6055e27873f69499f185
zh-deng <heromemo@web.de>
2025-05-12 21:47:09 +0200

    add title change input to roadmap

--- src/components/DashboardRoadmap/DashboardRoadmap.tsx
+++ src/components/DashboardRoadmap/DashboardRoadmap.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./DashboardRoadmap.scss";
-import { closestCenter, DndContext, DragEndEvent } from "@dnd-kit/core";
+import {
+	closestCenter,
+	DndContext,
+	DragEndEvent,
+	PointerSensor,
+	useSensor,
+	useSensors,
+} from "@dnd-kit/core";
 import {
 	arrayMove,
 	SortableContext,
@@ -9,15 +16,18 @@ import {
 import { RoadMapItem } from "./RoadMapItem/RoadMapItem";
 import { Text } from "../Text/Text";
 import { FiDownload } from "react-icons/fi";
-import { FaLock, FaLockOpen } from "react-icons/fa";
+import { FaLock, FaLockOpen, FaCheck } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
 import { RoadmapItems } from "../../types/Roadmap";
+import { MdModeEditOutline } from "react-icons/md";
 
 type DashboardRoadmapProps = {
+	title: string;
 	roadmapItems: RoadmapItems;
 };
 
 export const DashboardRoadmap: React.FC<DashboardRoadmapProps> = ({
+	title = "TEST",
 	roadmapItems,
 }) => {
 	const { t } = useTranslation();
@@ -26,17 +36,25 @@ export const DashboardRoadmap: React.FC<DashboardRoadmapProps> = ({
 
 	const [isPublic, setIsPublic] = useState<boolean>(false);
 
+	const [editingTitle, setEditingTitle] = useState<boolean>(false);
+
+	const [titleInput, setTitleInput] = useState<string>(title);
+
+	const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setTitleInput(event.target.value);
+	};
+
+	const sensors = useSensors(
+		useSensor(PointerSensor, { activationConstraint: { distance: 5 } })
+	);
+
 	const handleDragEnd = (event: DragEndEvent) => {
 		const { active, over } = event;
 
 		if (over && active.id !== over.id) {
 			setItems((items) => {
-				const oldIndex = items.findIndex(
-					(item) => item.id.toString() === active.id
-				);
-				const newIndex = items.findIndex(
-					(item) => item.id.toString() === over.id
-				);
+				const oldIndex = items.findIndex((item) => item.id === active.id);
+				const newIndex = items.findIndex((item) => item.id === over.id);
 				return arrayMove(items, oldIndex, newIndex);
 			});
 		}
@@ -44,6 +62,10 @@ export const DashboardRoadmap: React.FC<DashboardRoadmapProps> = ({
 
 	const handleLock = () => {
 		setIsPublic(!isPublic);
+	};
+
+	const toggleEditingTitle = () => {
+		setEditingTitle(!editingTitle);
 	};
 
 	const RoadmapEmptyFallback = () => (
@@ -58,9 +80,32 @@ export const DashboardRoadmap: React.FC<DashboardRoadmapProps> = ({
 				<div className="roadmap-header">
 					<div className="roadmap-header-text">
 						<Text content="dashboard.left.headerText" isBold={true} />
-						<div className="roadmap-header-name">TestRoadmap</div>
+						{editingTitle ? (
+							<div className="roadmap-header-input">
+								<input
+									type="text"
+									value={titleInput}
+									onChange={handleTitleChange}
+								/>
+							</div>
+						) : (
+							<div className="roadmap-header-title">{titleInput}</div>
+						)}
 					</div>
 					<div className="roadmap-header-icons">
+						{editingTitle ? (
+							<FaCheck
+								size={18}
+								onClick={toggleEditingTitle}
+								title={t("dashboard.left.confirmTitleIcon")}
+							/>
+						) : (
+							<MdModeEditOutline
+								size={20}
+								onClick={toggleEditingTitle}
+								title={t("dashboard.left.editTitleIcon")}
+							/>
+						)}
 						{isPublic ? (
 							<FaLockOpen
 								size={20}
@@ -80,17 +125,18 @@ export const DashboardRoadmap: React.FC<DashboardRoadmapProps> = ({
 				{roadmapItems.length ? (
 					<div className="roadmap-content">
 						<DndContext
+							sensors={sensors}
 							collisionDetection={closestCenter}
 							onDragEnd={handleDragEnd}
 						>
 							<SortableContext
-								items={items}
+								items={items.map((item) => item.id)}
 								strategy={verticalListSortingStrategy}
 							>
 								{items.map((item) => (
 									<RoadMapItem
 										key={item.id}
-										id={item.id.toString()}
+										id={item.id}
 										content={item.content}
 									/>
 								))}
