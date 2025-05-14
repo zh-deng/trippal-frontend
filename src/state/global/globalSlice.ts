cf00c42263207e309c1bd63afaccfa965cd51bc4
zh-deng <heromemo@web.de>
2025-05-14 22:47:46 +0200

    add trip index to state and new method to remove trip

--- src/state/global/globalSlice.ts
+++ src/state/global/globalSlice.ts
@@ -5,12 +5,11 @@ import { Trip } from "../../types/Trip";
 interface GlobalState {
 	activeUser: UserInfo | null;
 	activeTripIndex: number | null;
-
 }
 
 const initialState: GlobalState = {
 	activeUser: null,
-	activeTripIndex: null
+	activeTripIndex: null,
 };
 
 const globalSlice = createSlice({
@@ -19,7 +18,7 @@ const globalSlice = createSlice({
 	reducers: {
 		setActiveUser: (state, action: PayloadAction<UserInfo>) => {
 			state.activeUser = action.payload;
-			state.activeTripIndex = state.activeUser.trips.length-1
+			state.activeTripIndex = state.activeUser.trips.length - 1;
 		},
 		setActiveTripIndex: (state, action: PayloadAction<number>) => {
 			state.activeTripIndex = action.payload;
@@ -30,11 +29,33 @@ const globalSlice = createSlice({
 		addNewTrip: (state, action: PayloadAction<Trip>) => {
 			if (state.activeUser) {
 				state.activeUser.trips.push(action.payload);
-				state.activeTripIndex = state.activeUser.trips.length-1
+				state.activeTripIndex = state.activeUser.trips.length - 1;
 			}
-		}
+		},
+		removeOldTrip: (state, action: PayloadAction<number>) => {
+			const tripId = action.payload;
+
+			if (state.activeUser && Array.isArray(state.activeUser.trips)) {
+				const tripIndex = state.activeUser.trips.findIndex(
+					(trip) => trip.id === tripId
+				);
+
+				if (tripIndex !== -1) {
+					state.activeUser.trips.splice(tripIndex, 1);
+					if (tripIndex === state.activeTripIndex) {
+						state.activeTripIndex = state.activeUser.trips.length - 1;
+					}
+				}
+			}
+		},
 	},
 });
 
-export const { setActiveUser, logoutActiveUser, addNewTrip, setActiveTripIndex } = globalSlice.actions;
+export const {
+	setActiveUser,
+	logoutActiveUser,
+	addNewTrip,
+	setActiveTripIndex,
+	removeOldTrip,
+} = globalSlice.actions;
 export default globalSlice.reducer;
