a85cc2753b660a2f37de1297f127b8a8ff5d74c7
zh-deng <heromemo@web.de>
2025-05-18 20:23:42 +0200

    add hero to home page component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import "./Home.scss";
 import { RootState } from "../../state/store";
+import { Text } from "../../components/Text/Text";
 
 export const Home = () => {
 	const activeUser = useSelector(
@@ -9,8 +10,15 @@ export const Home = () => {
 
 	return (
     <div className="home">
-      Home
-      <div>
+      <div className="home-container">
+        <div className="home-hero">
+          <span className="home-hero-header">
+            <Text content={"home.hero.header"} />
+          </span>
+          <span className="home-hero-description">
+            <Text content={"home.hero.description"} />
+          </span>
+        </div>
         {
           `Current user: ${JSON.stringify(activeUser)}`
         }
