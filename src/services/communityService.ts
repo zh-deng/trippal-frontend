8eb9e3b4b80a4d0eb4b8ac9007c678aeea736321
zh-deng <heromemo@web.de>
2025-06-02 22:44:29 +0200

    rewrite service to match new endpoints of community

--- src/services/communityService.ts
+++ src/services/communityService.ts
@@ -1,14 +1,28 @@
-export async function fetchCommunityTrips(page: number): Promise<any> {
-  const response = await fetch(`${import.meta.env.VITE_BACKEND_BASEURL}/api/community?page=${page}`, {
-    method: "GET",
-    credentials: 'include'
-  });
+export async function fetchCommunityTrips(
+	page: number,
+	country?: string | null
+): Promise<any> {
+	const countryParam = country
+		? `&countryName=${encodeURIComponent(country)}`
+		: "";
 
-  if (!response.ok) {
-    const errorText = await response.text();
-    throw new Error(`Fetching paginated community trip list failed: ${errorText}`);
-  }
+	const response = await fetch(
+		`${
+			import.meta.env.VITE_BACKEND_BASEURL
+		}/api/community?page=${page}${countryParam}`,
+		{
+			method: "GET",
+			credentials: "include",
+		}
+	);
 
-  const communityList: any = await response.json();
-  return communityList;
+	if (!response.ok) {
+		const errorText = await response.text();
+		throw new Error(
+			`Fetching paginated community trip list failed: ${errorText}`
+		);
+	}
+
+	const communityList: any = await response.json();
+	return communityList;
 }
