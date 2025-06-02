dab572a6375f1da3d6cf465d5130b102713f4dcd
zh-deng <heromemo@web.de>
2025-06-02 22:45:03 +0200

    add new icon to community items

--- src/pages/Community/CommunityItem/CommunityItem.tsx
+++ src/pages/Community/CommunityItem/CommunityItem.tsx
@@ -1,41 +1,45 @@
 import { RoadmapItem } from "../../../types/Roadmap";
 import { Trip } from "../../../types/Trip";
 import "./CommunityItem.scss";
-import { FaStar, FaCommentAlt  } from "react-icons/fa";
+import { FaStar, FaCommentAlt } from "react-icons/fa";
+import { IoOpenOutline } from "react-icons/io5";
 
 type CommunityItemProps = {
 	trip: Trip;
 };
 
 export const CommunityItem: React.FC<CommunityItemProps> = ({ trip }) => {
-  const roadmapItems: RoadmapItem[] = trip.roadmapItems ?? []
+	const roadmapItems: RoadmapItem[] = trip.roadmapItems ?? [];
 
-	return <div className="community-item">
-    <div className="community-item-title">
-      {
-        trip.title
-      }
-    </div>
-    <div className="community-item-data">
-      {
-        roadmapItems.map((roadmapItem, index) => {
-          return index < 6 ? <div className="data-item" key={roadmapItem.id}>
-            <span>{roadmapItem.title}</span>
-          </div> : <></>
-        })
-      }
-    </div>
-    <div className="community-item-metadata">
-      <div className="metadata-stars">
-        {/* { trip.stars ?? 0 }  */}
-        0
-        <FaStar />
-      </div>
-      <div className="metadata-comments">
-        {/* { trip.comments.length ?? 0 } */}
-        0 
-        <FaCommentAlt />
-      </div>
-    </div>
-  </div>;
+	return (
+		<div className="community-item">
+			<div className="community-item-title">
+				{trip.title}
+				<IoOpenOutline size={22} />
+			</div>
+			<div className="community-item-data">
+				{roadmapItems.map((roadmapItem, index) => {
+					return index < 6 ? (
+						<div className="data-item" key={roadmapItem.id}>
+							<span>{roadmapItem.title}</span>
+						</div>
+					) : (
+						<></>
+					);
+				})}
+			</div>
+			<div className="community-item-metadata">
+				<div className="metadata-stars">
+					{/* { trip.stars ?? 0 }  */}
+					0
+					<FaStar />
+				</div>
+				<div className="metadata-comments">
+					{/* { trip.comments.length ?? 0 } */}
+					0
+					<FaCommentAlt />
+				</div>
+			</div>
+		</div>
+	);
 };
